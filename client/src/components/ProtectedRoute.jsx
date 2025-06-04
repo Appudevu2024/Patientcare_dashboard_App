ea014a5f6ec67d7229632b378cdf65c565de48c3
Appudevu2024 <sandhyababu08@gmail.com>
2025-06-04 08:31:20 +0300

    protectedroutes updated

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -7,26 +7,47 @@ const useAuth = (allowedRoles) => {
   const [role, setRole] = useState('');
   const [loading, setLoading] = useState(true);
 
+  const getTokenFromCookies = () => {
+  const cookie = document.cookie;
+  for (let tokenName of ['Admin_token', 'doctor_token', 'staff_token']) {
+    const match = cookie.match(new RegExp(`${tokenName}=([^;]+)`));
+    if (match) return { token: match[1], tokenName };
+  }
+  return null;
+};
+
+
   useEffect(() => {
     
-    const tokens = ['Admin_token', 'doctor_token', 'staff_token'];
-    const tokenString = tokens
-    .map(name => document.cookie.split('; ').find(cookie => cookie.startsWith(name)))
-    .find(Boolean); 
-    console.log(tokenString);
+    // const tokens = ['Admin_token', 'doctor_token', 'staff_token'];
+    // const tokenString = tokens
+    // .map(name => document.cookie.split('; ').find(cookie => cookie.startsWith(name)))
+    // .find(Boolean); 
+    // console.log(tokenString);
     
-        if (tokenString) {
-          const token = tokenString.split('=')[1];
-          try {
-            const decoded = jwtDecode(token);
-            console.log(decoded.role);
+    //     if (tokenString) {
+    //       const token = tokenString.split('=')[1];
+    //       try {
+    //         const decoded = jwtDecode(token);
+    //         console.log(decoded.role);
             
-            setRole(decoded.role);
-            setIsAuthenticated(true);
-          } catch (err) {
-            console.error('Invalid token', err);
-          }
-        }
+    //         setRole(decoded.role);
+    //         setIsAuthenticated(true);
+    //       } catch (err) {
+    //         console.error('Invalid token', err);
+    //       }
+    //     }
+    const result = getTokenFromCookies();
+  if (result) {
+    try {
+      const decoded = jwtDecode(result.token);
+      console.log("Decoded Token:", decoded);
+      setRole(decoded.role.toLowerCase());
+      setIsAuthenticated(true);
+    } catch (err) {
+      console.error('Invalid token', err);
+    }
+  }
       
     setLoading(false);
   }, []);
