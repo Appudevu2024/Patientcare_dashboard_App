29111babbeab29384863ff1db47d949ed5aa30a1
Appudevu2024 <sandhyababu08@gmail.com>
2025-06-07 03:32:31 +0300

    routes changed

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,7 +1,7 @@
 import { Navigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 
-import { jwtDecode } from 'jwt-decode';
+import  jwt_Decode from 'jwt-decode';
 const useAuth = (allowedRoles) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [role, setRole] = useState('');
@@ -51,7 +51,7 @@ const useAuth = (allowedRoles) => {
     const result = getTokenFromCookies();
   if (result) {
     try {
-      const decoded = jwtDecode(result.token);
+      const decoded = jwt_Decode(result.token);
       console.log("Decoded Token:", decoded);
       setRole((decoded?.role||'').toLowerCase());
       setIsAuthenticated(true);
