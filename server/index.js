7a180fc849c94ad58062badc41a00b74013f1620
Appudevu2024 <sandhyababu08@gmail.com>
2025-06-09 12:24:51 +0300

    backend changed

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const cors=require('cors')
 const cookieParser= require('cookie-parser');
-const app = express();
+
 require('dotenv').config();
 
 const mongoose = require('mongoose');
 const apiRouter = require('./Routes');
-
+const app = express();
 // Middleware to parse JSON
 
 app.use(cors({
@@ -16,29 +16,7 @@ app.use(cors({
 }))
 app.use(cookieParser());
 app.use(express.json());
-
-
-mongoose.connect(process.env.MONGO_URI,{
-   useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverSelectionTimeoutMS: 15000,
-})
-.then(() => {
-  console.log('✅ Connected to MongoDB');
-})
-.catch((err) => {
-  console.error('MongoDB connection error:', err);
-});
-// Start server
-const port = process.env.PORT;
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
-
-
-
 app.use('/api',apiRouter);
-
 
 
 // Basic route
@@ -49,6 +27,31 @@ app.get('/', (req, res) => {
 app.get('/test', (req, res) => {
   res.status(200).json({ message: 'Backend working ✅' });
 });
+
+// Start server
+const port = process.env.PORT;
+
+mongoose.connect(process.env.MONGO_URI,{
+   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 15000,
+})
+.then(() => {
+  console.log('✅ Connected to MongoDB');
+  app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+})
+.catch((err) => {
+  console.error('MongoDB connection error:', err);
+});
+
+
+
+
+
+
 
 
 
