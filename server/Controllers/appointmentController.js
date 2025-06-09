fded1e3038b24452d0bb5c72b01d366bc491084d
Appudevu2024 <sandhyababu08@gmail.com>
2025-06-10 02:23:16 +0300

    apoointment controllerupdated

--- server/Controllers/appointmentController.js
+++ server/Controllers/appointmentController.js
@@ -46,7 +46,8 @@ const createAppointment = async (req, res) => {
   console.log('User in getAllAppointments:', req.user);
       let query = {};
       if (role === 'doctor') {
-        query = { doctor: id }; // Only show appointments for logged-in doctor
+        // query = { doctor: id }; // Only show appointments for logged-in doctor
+        query = { doctor: new mongoose.Types.ObjectId(id) };
       }
   
       const appointments = await appointmentDB.find(query).populate('doctor', '_id name');
