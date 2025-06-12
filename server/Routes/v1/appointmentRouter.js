93d83a63c4807be956532d9bb7656a00ca83e125
Appudevu2024 <sandhyababu08@gmail.com>
2025-06-12 11:40:28 +0300

    appo router updated

--- server/Routes/v1/appointmentRouter.js
+++ server/Routes/v1/appointmentRouter.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { createAppointment, getAllAppointments,getAppointmentById,updateAppointment,deleteAppointment } = require('../../Controllers/appointmentController');
+const { createAppointment, getAllAppointments,getAppointmentById,getDoctorAppointments,updateAppointment,deleteAppointment } = require('../../Controllers/appointmentController');
 const authAnyUser = require('../../Middlewares/authAnyUser');
+const authDoctor = require('../../Middlewares/authDoctor');
 
 // Allow admin, doctor, and staff to   view and edit appointments
+router.get('/by-doctor', authDoctor, getDoctorAppointments);
 router.post('/', authAnyUser, createAppointment);
 router.get('/:id',authAnyUser, getAppointmentById);
+router.get('/', authAnyUser, getAllAppointments);
 router.get('/', authAnyUser, getAllAppointments);
 router.put('/', authAnyUser, updateAppointment);
 router.delete('/', authAnyUser, deleteAppointment);
