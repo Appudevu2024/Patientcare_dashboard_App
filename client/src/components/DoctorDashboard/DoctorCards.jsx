41fa1d809d78e51c505a9727fbe264fa7bb9082f
Appudevu2024 <sandhyababu08@gmail.com>
2025-06-15 12:06:40 +0300

    again updated doctorcards

--- client/src/components/DoctorDashboard/DoctorCards.jsx
+++ client/src/components/DoctorDashboard/DoctorCards.jsx
@@ -88,10 +88,14 @@ const DoctorCards = ({ onBloodBankClick }) => {
         />
       </div>
       {isModalOpen && (
-  <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-    <div className="bg-white dark:bg-gray-900 text-black dark:text-white rounded-lg shadow-lg max-w-5xl max-h-[90vh] overflow-y-auto p-6">
-      <div className="flex justify-between items-center mb-4">
-        <h3 className="text-lg text-center font-bold">Blood Bank Information</h3>
+  // <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+  //   <div className="bg-white dark:bg-gray-900 text-black dark:text-white rounded-lg shadow-lg max-w-5xl max-h-[90vh] overflow-y-auto p-6">
+  //     <div className="flex justify-between items-center mb-4">
+  //       <h3 className="text-lg text-center font-bold">Blood Bank Information</h3>
+       <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+          <div className="bg-white rounded-lg shadow-lg max-w-5xl  max-h-[90vh] overflow-y-auto p-6">
+            <div className="flex justify-between items-center mb-4">
+              <h3 className="text-lg text-center font-bold">Blood Bank Information</h3>
         <button
           onClick={() => setIsModalOpen(false)}
           className="btn btn-sm btn-circle btn-ghost dark:text-white"
